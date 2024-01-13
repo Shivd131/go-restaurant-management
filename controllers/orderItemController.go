b154fa3c85d1e600171741102eee345a139a8ebf
Shivd131 <shivdeshpande131@gmail.com>
2024-01-13 13:54:49 +0530

    chore: add extra function

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -18,6 +21,10 @@ func GetOrderItemsByOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 	}
+}
+
+func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+
 }
 
 func CreateOrderItem() gin.HandlerFunc {
