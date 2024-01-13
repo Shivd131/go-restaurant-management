1efb1197b1513ee8a7c99f888e37ead4c8c1c3ab
Shivd131 <shivdeshpande131@gmail.com>
2024-01-13 11:16:11 +0530

    add db logic

---
+++ database/databaseConnection.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDb := "mongodb+srv://Shivd131:ExG3uwXDQBpePCk@cluster0.zy3ehbu.mongodb.net/?retryWrites=true&w=majority"
+
+	fmt.Print(MongoDb)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+
+	return collection
+}
